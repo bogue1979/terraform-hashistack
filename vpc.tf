f0c24c76899fcb779a395e4277ceebba5484427e
Daniel Rossbach <daniel.rossbach@meteogroup.com>
2017-04-24 10:46:22 +0200

    clean up files

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,22 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+module "vpc" {
+  source             = "./vpc"
+  region             = "eu-central-1"
+  name               = "Hashistack"
+  vpc-fullcidr       = "10.40.0.0/16"
+  Subnet-DMZ-A-CIDR  = "10.40.3.0/24"
+  Subnet-DMZ-B-CIDR  = "10.40.4.0/24"
+  Subnet-PRIV-A-CIDR = "10.40.1.0/24"
+  Subnet-PRIV-B-CIDR = "10.40.2.0/24"
+  DnsZoneName        = "${var.internal_domain}"
+  environment_tag    = "recovery"
+}
+
 resource "aws_security_group" "jumphost" {
   name = "jumphost"
   tags {
